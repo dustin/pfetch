4d17a96c820b26ddd7c8ab0bec0a3bdb1e9821b8
Dustin Sallings <dustin@spy.net>
2008-11-01 19:57:11 -0700

    Added post-fetch command hooks.

--- pfetch.py
+++ pfetch.py
@@ -7,17 +7,41 @@ Copyright (c) 2008  Dustin Sallings <dustin@spy.net>
 
 import os
 
+from twisted.internet import reactor
 from twisted.web import client
+
+class ProcessHandler(object):
+
+    def __init__(self, u):
+        self.url=u
+
+    def makeConnection(self, process):
+        print "Starting job for %s" % self.url
+
+    def childDataReceived(self, childFd, data):
+        print "Heard from child on fd#%d: %s" % (childFd, data)
+
+    def childConnectionLost(self, childFd):
+        pass
+
+    def processEnded(self, reason):
+        print "Finished job for %s" % self.url
 
 class Download(object):
 
-    def __init__(self, url, file):
+    def __init__(self, url, file, cmd, args):
         self.url=url
         self.file=file
         self.tmpfile = file + ".tmp"
+        self.cmd=cmd
+        self.args=args
 
     def __onComplete(self, v):
         os.rename(self.tmpfile, self.file)
+        if self.cmd:
+            e={'PFETCH_URL': self.url, 'PFETCH_FILE': self.file}
+            args=[self.cmd] + self.args
+            reactor.spawnProcess(ProcessHandler(self.url), self.cmd, args, e)
 
     def __call__(self):
         return client.downloadPage(self.url, self.tmpfile).addCallback(
--- pfetch.tac
+++ pfetch.tac
@@ -15,8 +15,22 @@ application = service.Application("pfetch")
 r=random.Random()
 doc=microdom.parse('urls.xml')
 
+def find_command(u):
+    cmd=None
+    args=[]
+
+    cmdNodes=u.getElementsByTagName("command")
+    if len(cmdNodes):
+        cmd=cmdNodes[0].attributes["path"]
+        for a in cmdNodes[0].getElementsByTagName("arg"):
+            args.append(a.firstChild().data)
+
+    return cmd, args
+
 for u in doc.getElementsByTagName("url"):
+    cmd, args = find_command(u)
     a=u.attributes
+    print "got", cmd, `args`, "for", a['href']
     freq=int(a['freq'])
-    lc=task.LoopingCall(pfetch.Download(a['href'], a['output']))
+    lc=task.LoopingCall(pfetch.Download(a['href'], a['output'], cmd, args))
     reactor.callLater(r.randint(0, freq), lc.start, freq)
